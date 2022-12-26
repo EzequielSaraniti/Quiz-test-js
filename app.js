f59f32b323103bd8b577436b03fb4c118f1371b8
EzequielSaraniti <eze.rc177@gmail.com>
2022-12-26 13:03:22 -0300

    finish quiz game

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ import { Quiz } from "./models/Quiz.js"
 import { UI } from "./models/UI.js"
 
 const renderPage = (quiz, ui) => {
+
+    if(quiz.isEnded()){
+        ui.showScore(quiz.score)
+    } else {
     ui.showQuestion(quiz.getCurrentQuestion().text)
     ui.showChoices(quiz.getCurrentQuestion().choices, (currentChoice) => {
         quiz.validateAndContinue(currentChoice)
-        console.log(quiz.score)
+
         renderPage(quiz, ui)
-    })
+    })}
 }
 
 const main = () => {
