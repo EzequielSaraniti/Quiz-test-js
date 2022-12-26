60f629fdb2960cca7c2845593b28870fcb0c8473
EzequielSaraniti <eze.rc177@gmail.com>
2022-12-26 10:26:50 -0300

    Build Data Questions

---
+++ Question.js
@@ -0,0 +1,12 @@
+export class Question {
+    constructor(text, choices, answer){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choice){
+        return choice === this.answer;
+    }
+    
+}
--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-import {data} from './data.js';
+import { data } from './data.js';
+import { Question } from './Question.js';
 
-console.log(data);
+const questiosArray = data.map(q => new Question(q.question, q.choices, q.answer))
+
+// const question = new Question(data[0].question, data[0].choices, data[0].answer)
+
+console.log(questiosArray)
+
+// const result = question.isCorrectAnswer('Machine Code')
